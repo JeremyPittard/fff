fa76b2591dff2c132fb53a58ae9379b62d8a043f
JeremyPittard <jeremy@jpittard.net>
2020-06-30 22:09:50 +0800

    second commit, basically done, may remove the refresh we shall see

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import "./styles.scss";
-console.log("hello world!");
+console.log(
+  "Happy birthday Laura! if anyone wishes to add to the feed, just do the old #ferreriflauntsforty on the gram"
+);
+theRedo();
+
+function theRedo() {
+
+    //quick and dirty way to reload feed. ideally with time I'd re init the scipt tag
+
+    setTimeout(() => {window.location.reload(false)}, 900000)
+
+
+}
